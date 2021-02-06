b727b6250eea81fcd7d8c2fa38ab8860f168c846
yejiahao <jyab2020@mymail.pomona.edu>
2021-02-06 16:55:31 +0800

    nothing changed

--- src/pages/live-manage/child-cpn/live-card/index.js
+++ src/pages/live-manage/child-cpn/live-card/index.js
@@ -1,17 +1,42 @@
 import { Card, Avatar } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
 import React, { memo } from 'react'
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { render } from '@testing-library/react';
 
 const { Meta } = Card;
 
+
+const Live = {
+    title: '直播活动sample1',
+    description: '2021-02-06 14:54',
+    alt: 'example',
+    src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+}
+
+const menu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+          3rd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item danger>a danger item</Menu.Item>
+    </Menu>
+    )
+
 export default memo(function index() {
-    const Live = {
-        title: '直播活动sample1',
-        description: '2021-02-06 14:54',
-        alt: 'example',
-        src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
-    }
-        
     return (
         <Card
             style={{ width: 300 }}
@@ -29,6 +54,11 @@ export default memo(function index() {
                 title={Live.title}
                 description={Live.description}
             />
+        <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            Hover me <DownOutlined />
+            </a>
+        </Dropdown>
         </Card>
     )
 })
