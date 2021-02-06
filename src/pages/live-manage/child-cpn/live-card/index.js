6c18dba5fb7aa61f914b8c0f12d244f26f46137a
layla <minytiny@163.com>
2021-02-06 16:50:52 +0800

    add drop down menu

--- src/pages/live-manage/child-cpn/live-card/index.js
+++ src/pages/live-manage/child-cpn/live-card/index.js
@@ -1,8 +1,31 @@
 import { Card, Avatar } from 'antd';
+import { Menu, Dropdown, Button } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
+
 import React, { memo } from 'react'
+import { CardWrapper } from './style';
 
 const { Meta } = Card;
+
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                1st menu item
+        </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                2nd menu item
+        </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                3rd menu item
+        </a>
+        </Menu.Item>
+    </Menu>
+);
 
 export default memo(function index() {
     const Live = {
@@ -11,24 +34,28 @@ export default memo(function index() {
         alt: 'example',
         src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
     }
-        
+
     return (
-        <Card
-            style={{ width: 300 }}
-            cover={
-                <img
-                    alt={Live.alt}
-                    src={Live.src}
+        <CardWrapper>
+            <Card
+                style={{ width: 300 }}
+                cover={
+                    <img
+                        alt={Live.alt}
+                        src={Live.src}
+                    />
+                }
+                actions={[
+                    <Dropdown overlay={menu} placement="topCenter" arrow>
+                        <EllipsisOutlined key="ellipsis" />
+                    </Dropdown>
+                ]}
+            >
+                <Meta
+                    title={Live.title}
+                    description={Live.description}
                 />
-            }
-            actions={[
-                <EllipsisOutlined key="ellipsis" />
-            ]}
-        >
-            <Meta
-                title={Live.title}
-                description={Live.description}
-            />
-        </Card>
+            </Card>
+        </CardWrapper>
     )
 })
---
+++ src/pages/live-manage/child-cpn/live-card/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const CardWrapper = styled.div`
+  .ant-card-actions {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+`
