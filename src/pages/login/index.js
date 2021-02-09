8e0a02e6daba69fc1616ad2afae2948498c9a505
yejiahao <jyab2020@mymail.pomona.edu>
2021-02-09 20:16:48 +0800

    set up trivial log-in page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,11 @@
 import React, { memo } from 'react'
+import LoginForm from './LoginForm'
 
 export default memo(function Login() {
     return (
         <div>
             <h2>登录</h2>
+            <LoginForm />
         </div>
     )
 })
