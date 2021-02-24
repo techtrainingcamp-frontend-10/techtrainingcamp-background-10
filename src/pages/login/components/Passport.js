a202416822ee960b8e0ca5d97209060d1ceaf440
Fly3949 <i@fly.moe>
2021-02-24 15:22:12 +0800

    Update Passport.js

--- src/pages/login/components/Passport.js
+++ src/pages/login/components/Passport.js
@@ -30,7 +30,7 @@ export default class Passport {
         // }
         login(username, password).then(res => {
             console.log(res);
-            if (!res.data.success) { throw (new Error('login failed')) }
+            if (!res.success) { throw (new Error('login failed')) }
             this.isLogin = true;
             callback(res);
         }).catch(err => {
